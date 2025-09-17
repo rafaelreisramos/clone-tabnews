dad061142d0a3ad8d38de89f279b1645e5abfd75
Rafael Reis Ramos <rrramos@gmail.com>
2025-09-17 23:06:34 +0000

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 import database from "infra/database.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
+import password from "models/password.js";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -50,6 +52,11 @@ async function create(userInputValues) {
         action: "Utilize outro apelido para realizar o cadastro.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
   }
 
   async function runInsertQuery(userInputValues) {
