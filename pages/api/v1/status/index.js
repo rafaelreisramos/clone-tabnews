fb34207e42a61d65bb3f1125c867a4d1dbe2a615
rafaelreisramos <rrramos@gmail.com>
2026-02-09 15:47:24 -0300

    feat: standardize API responses and SQL queries

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -14,18 +14,18 @@ async function getHandler(request, response) {
   const userTryingToGet = request.context.user;
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections",
+    "SHOW max_connections;",
   );
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
   const databaseOpenedConnectionsValue =
@@ -48,5 +48,5 @@ async function getHandler(request, response) {
     statusObject,
   );
 
-  return response.status(200).json(secureOutputValues);
+  response.status(200).json(secureOutputValues);
 }
