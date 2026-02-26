cc04f8170cfe7c2ff7755bde1de26899ba68076b
rafaelreisramos <rrramos@gmail.com>
2026-02-26 12:36:38 -0300

    refactor: improve error classes with default constructor values

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,14 @@
 export class InternalServerError extends Error {
-  constructor({ cause, statusCode }) {
-    super("Um erro interno não esperado aconteceu.", {
-      cause,
-    });
+  constructor({
+    cause,
+    message = "Um erro interno não esperado aconteceu.",
+    action = "Entre em contato com o suporte.",
+    statusCode = 500,
+  }) {
+    super(message, { cause });
     this.name = "InternalServerError";
-    this.action = "Entre em contato com o suporte.";
-    this.statusCode = statusCode || 500;
+    this.action = action;
+    this.statusCode = statusCode;
   }
 
   toJSON() {
@@ -19,13 +22,16 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
-    super(message || "Serviço indisponível no momento.", {
-      cause,
-    });
+  constructor({
+    cause,
+    message = "Serviço indisponível no momento.",
+    action = "Verifique se o serviço está disponível.",
+    statusCode = 503,
+  }) {
+    super(message, { cause });
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível.";
-    this.statusCode = 503;
+    this.action = action;
+    this.statusCode = statusCode;
   }
 
   toJSON() {
@@ -39,12 +45,14 @@ export class ServiceError extends Error {
 }
 
 export class ValidationError extends Error {
-  constructor({ cause, message, action }) {
-    super(message || "Um erro de validação ocorreu.", {
-      cause,
-    });
+  constructor({
+    cause,
+    message = "Um erro de validação ocorreu.",
+    action = "Ajuste os dados enviados e tente novamente.",
+  }) {
+    super(message, { cause });
     this.name = "ValidationError";
-    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.action = action;
     this.statusCode = 400;
   }
 
@@ -59,13 +67,14 @@ export class ValidationError extends Error {
 }
 
 export class NotFoundError extends Error {
-  constructor({ cause, message, action }) {
-    super(message || "Não foi possível encontrar este recurso no sistema.", {
-      cause,
-    });
+  constructor({
+    cause,
+    message = "Não foi possível encontrar este recurso no sistema.",
+    action = "Verifique se os parâmetros enviados na consulta estão certos.",
+  }) {
+    super(message, { cause });
     this.name = "NotFoundError";
-    this.action =
-      action || "Verifique se os parâmetros enviados na consulta estão certos.";
+    this.action = action;
     this.statusCode = 404;
   }
 
@@ -80,13 +89,14 @@ export class NotFoundError extends Error {
 }
 
 export class ForbiddenError extends Error {
-  constructor({ cause, message, action }) {
-    super(message || "Acesso negado.", {
-      cause,
-    });
+  constructor({
+    cause,
+    message = "Acesso negado.",
+    action = "Verifique as features necessárias antes de continuar.",
+  }) {
+    super(message, { cause });
     this.name = "ForbiddenError";
-    this.action =
-      action || "Verifique as features necessárias antes de continuar.";
+    this.action = action;
     this.statusCode = 403;
   }
 
@@ -101,12 +111,14 @@ export class ForbiddenError extends Error {
 }
 
 export class UnauthorizedError extends Error {
-  constructor({ cause, message, action }) {
-    super(message || "Usuário não autenticado.", {
-      cause,
-    });
+  constructor({
+    cause,
+    message = "Usuário não autenticado.",
+    action = "Faça novamente o login para continuar.",
+  }) {
+    super(message, { cause });
     this.name = "UnauthorizedError";
-    this.action = action || "Faça novamente o login para continuar.";
+    this.action = action;
     this.statusCode = 401;
   }
 
