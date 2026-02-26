45f2de86c08fcb5a2a85b5e9846d6d974002288d
rafaelreisramos <rrramos@gmail.com>
2026-02-26 11:14:45 -0300

    fix: convert `POSTGRES_PORT` and `EMAIL_SMTP_PORT` to numbers

--- infra/email.js
+++ infra/email.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
-  port: process.env.EMAIL_SMTP_PORT,
+  port: Number(process.env.EMAIL_SMTP_PORT),
   auth: {
     user: process.env.EMAIL_SMTP_USER,
     pass: process.env.EMAIL_SMTP_PASSWORD,
